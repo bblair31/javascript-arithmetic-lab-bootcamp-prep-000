36e37d03c2b2095faa092c806d447dab1a018a55
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-21 16:30:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var a, b
 function add (a, b) {
   return (a + b)
 }
-function subtract(a, b) {
+function subtract (a, b) {
   return (a - b)
+}
+function multiply (a, b) {
+  return (a * b)
+}
+function divide (a, b) {
+  return (a / b)
 }
